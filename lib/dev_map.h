6a478dd39cdb5053f4e01153cc23e88c037a0a61
nikigor <n.gorbadey@gmail.com>
2019-05-15 16:50:51 +0300

    manipulators: change number of prox sensors to 5
    Signed-off-by: nikigor <n.gorbadey@gmail.com>

--- lib/dev_map.h
+++ lib/dev_map.h
@@ -2,7 +2,7 @@
 #define _DEV_MAP_H_
 
 #define NUMBER_OF_DYNAMIXELS            4
-#define NUMBER_OF_PROX_SENSORS          6
+#define NUMBER_OF_PROX_SENSORS          5
 #define NUMBER_OF_STEP_MOTORS           1
 
 #endif //_DEV_MAP_H_
